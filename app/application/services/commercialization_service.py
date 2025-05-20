4a8c03a70b17374514a6f7fb7f877b7636edb8b7
henrique.comp <henrique.comp@gmail.com>
2025-05-20 20:06:22 -0300

    busca em arquivos csv

--- app/application/services/commercialization_service.py
+++ app/application/services/commercialization_service.py
@@ -1,10 +1,10 @@
-from infrastructure.repositories.comercio_csv import retornar_comercio_csv
 from infrastructure.external_services.commercialization_scrape import (
     CommercializationScrape,
 )
 from application.DTOs.commercialization_response import CommercializationResponse
 from application.common.config import Config
 from application.common.url_handler import UrlHandler
+from infrastructure.repositories.commercialization_csv import CommercializationCSV
 
 
 class CommercializationService:
@@ -13,6 +13,8 @@ class CommercializationService:
         self, year: int
     ) -> list[CommercializationResponse]:
         try:
+
+            division = 1 / 0
             data = []
             config = Config().get_config("Commercialization")
             for item in config:
@@ -29,18 +31,17 @@ class CommercializationService:
                     )
             return data
         except Exception as e:
-            # Vou chamar infrastructure/repositories/production_csv.py
             data = []
             config = Config().get_config("Commercialization")
-            for item in config:
-                results = retornar_comercio_csv(item.path, item.category, year)
+            for item in config:                
+                results = CommercializationCSV().get_commercialization_by_year_csv(item.file, item.category, year)
                 for item in results:
                     data.append(
                         CommercializationResponse(
                             category=item.category,
                             name=item.name,
-                            quantity=item.quantity,
+                            quantity=item.quantity
                         )
                     )
-            return data
 
+            return data
---
+++ app/infrastructure/repositories/commercialization_csv.py
@@ -0,0 +1,21 @@
+from application.DTOs.commercialization_response import CommercializationResponse
+import pandas as pd
+
+
+class CommercializationCSV:
+
+    def get_commercialization_by_year_csv(
+        self, file_path, category, year
+    ) -> list[CommercializationResponse]:
+        df = pd.read_csv(file_path, delimiter=";")
+        data = []
+        for _, row in df.iterrows():
+            data.append(
+                CommercializationResponse(
+                    category=category,
+                    name=str(row["Produto"]),
+                    quantity=int(row[f"{year}"]),
+                )
+            )
+
+        return data
